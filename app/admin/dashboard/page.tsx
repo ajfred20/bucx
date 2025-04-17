be61552a7015ee4cb1bde9262443d3a2f322b67b
Fred Aj <138402744+ajfred20@users.noreply.github.com>
2025-04-17 14:08:50 +0100

    updated dashboard

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2 className="text-3xl text-center tracking-tighter font-semibold">
+        This s the dashboard route
+      </h2>
+    </div>
+  );
+};
+
+export default Dashboard;
