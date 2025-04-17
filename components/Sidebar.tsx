ee04c5e922801a7485e2fb6d96852cb4fc88b806
Fred Aj <138402744+ajfred20@users.noreply.github.com>
2025-04-17 23:18:19 +0100

    updated sidebar by adding svgs

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,14 +1,5 @@
 import Link from "next/link";
 import Image from "next/image";
-import {
-  HomeIcon,
-  SquaresPlusIcon,
-  ClockIcon,
-  UsersIcon,
-  CurrencyDollarIcon,
-  ArrowRightIcon,
-  WalletIcon,
-} from "@heroicons/react/24/outline";
 
 export default function Sidebar() {
   return (
@@ -76,7 +67,21 @@ export default function Sidebar() {
               href="/admin/deposits"
               className="flex items-center gap-3 px-4 py-2 text-gray-700 hover:bg-gray-100"
             >
-              <SquaresPlusIcon className="w-5 h-5" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z"
+                />
+              </svg>
+
               <span>Deposits</span>
             </Link>
           </li>
@@ -85,7 +90,21 @@ export default function Sidebar() {
               href="/admin/withdrawals"
               className="flex items-center gap-3 px-4 py-2 text-gray-700 hover:bg-gray-100"
             >
-              <ClockIcon className="w-5 h-5" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z"
+                />
+              </svg>
+
               <span>Withdrawals</span>
             </Link>
           </li>
@@ -94,7 +113,21 @@ export default function Sidebar() {
               href="/admin/transaction-logs"
               className="flex items-center gap-3 px-4 py-2 text-gray-700 hover:bg-gray-100"
             >
-              <ClockIcon className="w-5 h-5 rotate-90" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z"
+                />
+              </svg>
+
               <span>Transaction logs</span>
             </Link>
           </li>
@@ -109,7 +142,21 @@ export default function Sidebar() {
               href="/admin/users"
               className="flex items-center gap-3 px-4 py-2 text-gray-700 hover:bg-gray-100"
             >
-              <SquaresPlusIcon className="w-5 h-5" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"
+                />
+              </svg>
+
               <span>Users</span>
             </Link>
           </li>
@@ -118,7 +165,21 @@ export default function Sidebar() {
               href="/admin/withdrawals-users"
               className="flex items-center gap-3 px-4 py-2 text-gray-700 hover:bg-gray-100"
             >
-              <ClockIcon className="w-5 h-5" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z"
+                />
+              </svg>
+
               <span>Withdrawals</span>
             </Link>
           </li>
@@ -127,7 +188,21 @@ export default function Sidebar() {
               href="/admin/transaction-logs-users"
               className="flex items-center gap-3 px-4 py-2 text-gray-700 hover:bg-gray-100"
             >
-              <ClockIcon className="w-5 h-5 rotate-90" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z"
+                />
+              </svg>
+
               <span>Transaction logs</span>
             </Link>
           </li>
@@ -136,7 +211,21 @@ export default function Sidebar() {
               href="/admin/waitlist-users"
               className="flex items-center gap-3 px-4 py-2 bg-gray-900 text-white"
             >
-              <UsersIcon className="w-5 h-5" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+                />
+              </svg>
+
               <span>Waitlist users</span>
             </Link>
           </li>
@@ -151,7 +240,21 @@ export default function Sidebar() {
               href="/admin/usd-account"
               className="flex items-center gap-3 px-4 py-2 text-gray-700 hover:bg-gray-100"
             >
-              <SquaresPlusIcon className="w-5 h-5" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+
               <span>USD account</span>
             </Link>
           </li>
@@ -160,7 +263,21 @@ export default function Sidebar() {
               href="/admin/solana-wallet"
               className="flex items-center gap-3 px-4 py-2 text-gray-700 hover:bg-gray-100"
             >
-              <ClockIcon className="w-5 h-5" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3"
+                />
+              </svg>
+
               <span>Solana wallet</span>
             </Link>
           </li>
@@ -169,7 +286,21 @@ export default function Sidebar() {
               href="/admin/transaction-logs-account"
               className="flex items-center gap-3 px-4 py-2 text-gray-700 hover:bg-gray-100"
             >
-              <ClockIcon className="w-5 h-5 rotate-90" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z"
+                />
+              </svg>
+
               <span>Transaction logs</span>
             </Link>
           </li>
@@ -178,7 +309,21 @@ export default function Sidebar() {
               href="/admin/waitlist-users-account"
               className="flex items-center gap-3 px-4 py-2 text-gray-700 hover:bg-gray-100"
             >
-              <ClockIcon className="w-5 h-5 rotate-90" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z"
+                />
+              </svg>
+
               <span>Waitlist users</span>
             </Link>
           </li>
@@ -190,7 +335,7 @@ export default function Sidebar() {
         <div className="flex items-center gap-3">
           <div className="h-10 w-10 rounded overflow-hidden">
             <Image
-              src="/profile.jpg"
+              src="/flags/uk.png"
               alt="User Avatar"
               width={40}
               height={40}
@@ -201,7 +346,6 @@ export default function Sidebar() {
             <h3 className="font-medium text-gray-900">Slazenger</h3>
             <p className="text-xs text-gray-500">slazenger@gmail.com</p>
           </div>
-          <ArrowRightIcon className="w-5 h-5 text-gray-400" />
         </div>
       </div>
     </aside>
