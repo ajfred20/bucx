29feea0408ed600318c6e8a1dbc65d63e4e35a1d
Fred Aj <138402744+ajfred20@users.noreply.github.com>
2025-04-17 23:23:48 +0100

    updated dashboard

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Dashboard = () => {
   return (
     <div>
-      <h2 className="text-3xl text-center tracking-tighter font-semibold">
+      <h2 className="text-7xl text-center tracking-tighter text-black font-semibold">
         This is the dashboard route
       </h2>
     </div>
